0ab9fac3fbab0ae4be60b9ed171373deac14fe92
Wakana530 <05wkn30@gmail.com>
2017-07-01 19:39:14 +0900

    back to top
    back to top

--- js/script.js
+++ js/script.js
@@ -28,3 +28,23 @@ $('.page3-image-box3').hover(function(){
 });
 
 new WOW().init();
+
+$(function() {
+    var topBtn = $('#page-top');    
+    topBtn.hide();
+    //スクロールが100に達したらボタン表示
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            topBtn.fadeIn();
+        } else {
+            topBtn.fadeOut();
+        }
+    });
+    //スクロールしてトップ
+    topBtn.click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+});
